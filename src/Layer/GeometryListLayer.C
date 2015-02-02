7d8f0c69fc5089614660e22923781dd40c4d6272
Andrew Gilbert <andrew.gilbert@anu.edu.au>
2015-02-02 13:54:38 +1100

    Removed redundant line

--- src/Layer/GeometryListLayer.C
+++ src/Layer/GeometryListLayer.C
@@ -38,6 +38,7 @@ using namespace qglviewer;
 namespace IQmol {
 namespace Layer {
 
+// label looks like it needs deprecating
 GeometryList::GeometryList(Data::GeometryList const& geometryList, QString const& label)
  : Base(geometryList.label()), m_configurator(*this), m_geometryList(geometryList),
    m_speed(0.125), m_reperceiveBonds(false), m_bounce(false), m_loop(false)
@@ -47,10 +48,7 @@ GeometryList::GeometryList(Data::GeometryList const& geometryList, QString const
    Data::GeometryList::const_iterator iter;
    for (iter = m_geometryList.begin(); iter != m_geometryList.end(); ++iter) {
        Data::Geometry* geometry(const_cast<Data::Geometry*>(*iter));
-       if (geometry) {
-          Data::Energy& energy(geometry->getProperty<Data::TotalEnergy>());
-         appendRow(new Layer::Geometry(*geometry));
-       }
+       if (geometry) appendRow(new Layer::Geometry(*geometry));
    }
 
    m_configurator.load();
