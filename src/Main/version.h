3ba851e6b85bf33081b9398159dd87f857e6166d
Andrew Gilbert <andrew.gilbert@anu.edu.au>
2021-07-14 22:32:20 +1000

    2.15.2 windows version

--- src/Main/version.h
+++ src/Main/version.h
@@ -1,5 +1,5 @@
-// Version file generated on "Fri 21 May 2021 23:59:39 AEST"
+// Version file generated on "Wed, Jul 14, 2021  9:59:39 PM"
 // Automatically generated file, do not edit
 #define IQMOL_YEAR "2021"
-#define IQMOL_VERSION "2.15.1"
-#define GIT_VERSION "2.15.0-4-gc314209"
+#define IQMOL_VERSION "2.15.2"
+#define GIT_VERSION "2.15.0-15-gefc7a27"
